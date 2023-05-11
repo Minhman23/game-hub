b0a9a05dca2c98b79738ea7c16f337b470673837
Minhman23 <dominhman2018@gmail.com>
2023-05-11 10:25:04 +0700

    Add search function

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,17 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import Logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { GameQuery } from "../App";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (search: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
-    <HStack  padding={"10px"}>
+    <HStack padding={"10px"}>
       <Image src={Logo} boxSize={"60px"}></Image>
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
