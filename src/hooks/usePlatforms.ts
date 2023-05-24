6088c20bf3274dab43c407cede5468c9c800356c
Minhman23 <dominhman2018@gmail.com>
2023-05-24 23:50:35 +0700

    Fix import error

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
-import ms from "ms";
-import { Platform } from "./entities/Platform";
+import Platform from "./entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms");
 
